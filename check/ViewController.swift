b015c5d0741eb06882a5d08150079596618aa50a
zohaibbenjamin <zohaibbenjamin@gmail.com>
2023-04-25 20:41:05 +0500

    Initial project setup

---
+++ check/ViewController.swift
@@ -0,0 +1,21 @@
+//
+//  ViewController.swift
+//  check
+//
+//  Created by Apple on 25/04/2023.
+//
+
+import UIKit
+import testLibrryCocoapod
+
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ testLibrryCocoapod/testLibrryCocoapod.h
@@ -0,0 +1,18 @@
+//
+//  testLibrryCocoapod.h
+//  testLibrryCocoapod
+//
+//  Created by Apple on 25/04/2023.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for testLibrryCocoapod.
+FOUNDATION_EXPORT double testLibrryCocoapodVersionNumber;
+
+//! Project version string for testLibrryCocoapod.
+FOUNDATION_EXPORT const unsigned char testLibrryCocoapodVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <testLibrryCocoapod/PublicHeader.h>
+
+
